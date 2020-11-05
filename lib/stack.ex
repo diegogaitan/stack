2074331aa3ffb323833bc6a71de4e3c3014543ae
Diego Gaitán <bairesdev_dgaitan@iseatz.com>
2020-11-04 21:16:43 -0500

    Added API for the Stack and tests for it

--- lib/stack.ex
+++ lib/stack.ex
@@ -1,18 +1,11 @@
 defmodule Stack do
-  @moduledoc """
-  Documentation for `Stack`.
-  """
+  alias Stack.Server
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Stack.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def start_link(initial_state) when is_list(initial_state) do
+    GenServer.start_link(Server, initial_state)
   end
+
+  def state(stack), do: GenServer.call(stack, :state)
+  def push(stack, element), do: GenServer.cast(stack, {:push, element})
+  def pop(stack), do: GenServer.call(stack, :pop)
 end
--- test/stack_test.exs
+++ test/stack_test.exs
@@ -1,7 +1,36 @@
 defmodule StackTest do
   use ExUnit.Case
 
-  test "greets the world" do
-    assert Stack.hello() == :world
+  setup do
+    stack = start_supervised!(
+        %{
+           id: Stack,
+           start: {Stack, :start_link, [[]]}
+         }
+    )
+    %{:stack => stack}
   end
+
+  test "state", %{:stack => stack} do
+    assert Stack.state(stack) == []
+  end
+
+  test "push", %{:stack => stack} do
+    Stack.push(stack, 9)
+    Stack.push(stack, 10)
+    Stack.push(stack, 15)
+
+    assert Stack.state(stack) == [15, 10, 9]
+  end
+
+  test "pop", %{:stack => stack} do
+    Stack.push(stack, 9)
+    Stack.push(stack, 10)
+    Stack.push(stack, 15)
+
+    assert Stack.pop(stack) == 15
+    assert Stack.state(stack) == [10, 9]
+  end
+
+
 end
